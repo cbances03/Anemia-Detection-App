bc6fb1253b41f6f9d3710e6c842d7da8b0cc44cf
cbances03 <claudio.bances03@gmail.com>
2026-07-30 22:41:48 -0500

    Update predict.py

--- backend/app/api/predict.py
+++ backend/app/api/predict.py
@@ -1,1 +1,11 @@
+from fastapi import APIRouter
 
+from app.services.prediction_service import PredictionService
+
+router = APIRouter(tags=["Prediction"])
+
+
+@router.post("/predict")
+def predict():
+
+    return PredictionService.predict()
