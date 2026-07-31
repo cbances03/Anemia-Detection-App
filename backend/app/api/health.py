5439cc9037f9eb97119324e3b7a065efa5afbbb8
cbances03 <claudio.bances03@gmail.com>
2026-07-30 22:41:16 -0500

    Update health.py

--- backend/app/api/health.py
+++ backend/app/api/health.py
@@ -1,1 +1,9 @@
+from fastapi import APIRouter
+from app.schemas.responses import HealthResponse
 
+router = APIRouter(tags=["Health"])
+
+
+@router.get("/health", response_model=HealthResponse)
+def health():
+    return HealthResponse(status="healthy")
