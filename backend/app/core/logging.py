432e3ec53a029470ab6175a786a3353ac33e3df0
cbances03 <claudio.bances03@gmail.com>
2026-07-30 22:40:50 -0500

    Update logging.py

--- backend/app/core/logging.py
+++ backend/app/core/logging.py
@@ -1,1 +1,12 @@
+import logging
+import os
 
+os.makedirs("logs", exist_ok=True)
+
+logging.basicConfig(
+    filename="logs/backend.log",
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(message)s"
+)
+
+logger = logging.getLogger("backend")
